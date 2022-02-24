5e9c00cccee4a1286a58eb926f4145d908afd0da
dylanshih1 <44419396+dylanshih1@users.noreply.github.com>
2022-02-24 16:40:40 -0500

    Add files via upload

---
+++ qb_test.py
@@ -0,0 +1,76 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn import metrics
+
+if __name__ == '__main__':
+    data = pd.read_csv('QBs.csv')
+
+
+    ### 0.5 FP ###
+    # OK correlation - decent predictive power
+
+    data = data.drop(["Name", "Team", "CL", "Position", "G", "Pass Att", "Pass Com", "Int", "Pass TD",	"Pass Yds"], axis=1)
+    x = data[['Pass Yds/Game', 'TDs/Game', 'Ints/Game', 'TD/Int', 'ComPer']]
+    y = data['FPts/Game']
+    print(data.shape)
+
+
+    #Correlation
+    print(data.corr(method='pearson')) # Pearson's r - look at last row
+    
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size= 0.25, random_state=0)
+    # Regression
+    regressor = LinearRegression()
+    regressor.fit(x_train, y_train)
+    coeff_df = pd.DataFrame(regressor.coef_, x.columns, columns=['Coefficient'])
+    print(coeff_df)
+
+    # Prediction
+    y_pred = regressor.predict(x_test)
+    df = pd.DataFrame({'Actual': y_test, 'Predicted': y_pred})
+    print(df)
+
+    # Analysis
+    print(data.describe())
+    print('Mean Absolute Error:', metrics.mean_absolute_error(y_test, y_pred))
+    print('Mean Squared Error:', metrics.mean_squared_error(y_test, y_pred))
+    print('Root Mean Squared Error:', np.sqrt(metrics.mean_squared_error(y_test, y_pred)))
+
+    '''
+    ##### DYAR #####
+    # Better correlation - worse RMSE for prediction
+    
+    data = pd.read_csv('wr_stats/cfb_nfl_wr.csv')
+
+    data = data.drop(["player", "school", "conf", "team", 'g', 'avg', 'points'], axis=1)
+    data = data.drop(data[data.dyar == 0].index)
+    x = data[['yds', 'rec', 'td']]
+    y = data['dyar']
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.25, random_state=0)
+
+    # Correlation
+    print(data.corr(method='pearson'))  # Pearson's r - look at last row
+
+    # Regression
+    regressor = LinearRegression()
+    regressor.fit(x_train, y_train)
+    coeff_df = pd.DataFrame(regressor.coef_, x.columns, columns=['Coefficient'])
+    print(coeff_df)
+
+    # Prediction
+    y_pred = regressor.predict(x_test)
+    df = pd.DataFrame({'Actual': y_test, 'Predicted': y_pred})
+    print(df)
+
+    # Analysis
+    print(data.describe())
+    print('Mean Absolute Error:', metrics.mean_absolute_error(y_test, y_pred))
+    print('Mean Squared Error:', metrics.mean_squared_error(y_test, y_pred))
+    print('Root Mean Squared Error:', np.sqrt(metrics.mean_squared_error(y_test, y_pred)))
+    '''
+
+
+
